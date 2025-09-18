5387ac154cbb5a2558d0b101288a2b639cd4da07
DGPlay Code <dgamerplayer722@gmail.com>
2025-09-17 22:55:16 -0300

    Projeto Start

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import "./src/styles/global.css";
+import { StatusBar } from 'expo-status-bar';
+import { Profile } from '@/app/Profile';
+
+export default function App() {
+  return (
+    <>
+      <Profile />
+      <StatusBar style="auto" />
+    </>
+  );
+}
